f90a8f16b0255680f7bb72d3ca830391a4503c44
kishore <kishore.9980602683@gmail.com>
2021-06-25 00:39:21 +0530

    checkpoint4

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -4,6 +4,7 @@ import Details from './details/Details';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BookShow from '../screens/bookshow/BookShow';
 import moviesData from '../assets/moviesData';
+import Confirmation from './confirmation/Confirmation';
 
 class Controller extends Component {
 
@@ -15,6 +16,7 @@ class Controller extends Component {
         <Route exact path='/' render={(props) => <Home {...props} movieData={moviesData} />} />
           <Route path='/movie/:id' render={(props) => <Details {...props}  />} />
           <Route path='/bookshow/:id' render={(props) => <BookShow {...props}  />} />
+          <Route path='/confirm/:id' render={(props) => <Confirmation {...props} />} />
         </div>
       </Router>
     )
