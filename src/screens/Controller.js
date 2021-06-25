13f73c8f3ac0863bbed7efeb8cfb5ae3ab4b4d2a
kishore <kishore.9980602683@gmail.com>
2021-06-25 23:35:12 +0530

    checkpoint5

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -7,13 +7,14 @@ import moviesData from '../assets/moviesData';
 import Confirmation from './confirmation/Confirmation';
 
 const Controller =()=> {
+  const baseUrl = 'http://localhost:8085/api/v1';
     return (
       <Router>
         <div className="main-container">
-        <Route exact path='/' render={(props) => <Home {...props} movieData={moviesData} />} />
-          <Route path='/movie/:id' render={(props) => <Details {...props}  />} />
-          <Route path='/bookshow/:id' render={(props) => <BookShow {...props}  />} />
-          <Route path='/confirm/:id' render={(props) => <Confirmation {...props} />} />
+        <Route exact path='/' render={(props) => <Home {...props} movieData={moviesData} baseUrl={ baseUrl} />} />
+          <Route path='/movie/:id' render={(props) => <Details {...props} baseUrl={ baseUrl} />} />
+          <Route path='/bookshow/:id' render={(props) => <BookShow {...props} baseUrl={ baseUrl} />} />
+          <Route path='/confirm/:id' render={(props) => <Confirmation {...props} baseUrl={ baseUrl} />} />
         </div>
       </Router>
     )
